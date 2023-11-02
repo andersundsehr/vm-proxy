8716c60854c7b90cf6399a6d444eef3267e89f72
Matthias Vogel <m.vogel@andersundsehr.com>
2023-11-02 14:16:35 +0000

    ✨ add ServerAliveInterval 15 to client

--- client/autossh.sh
+++ client/autossh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-echo ssh -v -N -o ExitOnForwardFailure=yes -R $REMOTE_PORT:$DEST_HOST:$DEST_PORT $SSH_USER@$SSH_HOST -p$SSH_PORT
-autossh -M 0 -v -N -o ExitOnForwardFailure=yes -R $REMOTE_PORT:$DEST_HOST:$DEST_PORT $SSH_USER@$SSH_HOST -p$SSH_PORT
+echo ssh -v -N -o ExitOnForwardFailure=yes -o ServerAliveInterval=15 -R $REMOTE_PORT:$DEST_HOST:$DEST_PORT $SSH_USER@$SSH_HOST -p$SSH_PORT
+autossh -M 0 -v -N -o ExitOnForwardFailure=yes -o ServerAliveInterval=15 -R $REMOTE_PORT:$DEST_HOST:$DEST_PORT $SSH_USER@$SSH_HOST -p$SSH_PORT
