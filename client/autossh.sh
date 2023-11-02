ccd55e12f29277c52d63dc8636298143bc2fa7aa
Matthias Vogel <m.vogel@andersundsehr.com>
2023-11-02 14:43:41 +0100

    Update autossh.sh

--- client/autossh.sh
+++ client/autossh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-echo ssh -v -N -R -o ExitOnForwardFailure=yes $REMOTE_PORT:$DEST_HOST:$DEST_PORT $SSH_USER@$SSH_HOST -p$SSH_PORT
-autossh -M 0 -v -N -R -o ExitOnForwardFailure=yes $REMOTE_PORT:$DEST_HOST:$DEST_PORT $SSH_USER@$SSH_HOST -p$SSH_PORT
+echo ssh -v -N -o ExitOnForwardFailure=yes -R $REMOTE_PORT:$DEST_HOST:$DEST_PORT $SSH_USER@$SSH_HOST -p$SSH_PORT
+autossh -M 0 -v -N -o ExitOnForwardFailure=yes -R $REMOTE_PORT:$DEST_HOST:$DEST_PORT $SSH_USER@$SSH_HOST -p$SSH_PORT
