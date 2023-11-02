5d0b467102f8b93dd98399b6de59459841b33aeb
Matthias Vogel <m.vogel@andersundsehr.com>
2023-11-02 14:07:17 +0000

    ✨ add ClientAliveInterval 15 to server

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -5,8 +5,6 @@ if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
   ssh-keygen -A
 fi
 
-sed "s/AllowTcpForwarding .*/AllowTcpForwarding yes/g" /etc/ssh/sshd_config > /etc/ssh/sshd_config
-
 if [ ! -f /root/.ssh/authorized_keys ]; then
   echo $INITAL_AUTHORIZED_KEYS > /root/.ssh/authorized_keys
 fi
